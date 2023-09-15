f0e8a658b4ee71d783035fe80b8f7aad3636d654
Eduardo <eduardoarcunha12@gmail.com>
2023-09-15 07:52:16 -0300

    feat: enemy clock

--- Roll-a-Ball/Assets/Scripts/PanelManager.cs
+++ Roll-a-Ball/Assets/Scripts/PanelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class PanelManager : MonoBehaviour
@@ -9,12 +10,16 @@ public class PanelManager : MonoBehaviour
     [SerializeField] private GameObject gamePanel;
     [SerializeField] TextMeshProUGUI countText;
     [SerializeField] private GameObject popUpText;
+    [SerializeField] private Image enemyClock;
     
 
     [SerializeField] private GameObject engGamePanel;
     [SerializeField] private GameObject finalScoreText;
     [SerializeField] private GameObject finalText;
     [SerializeField] private GameObject button;
+
+    private float enemyCooldown = 5f;
+    private float enemyCooldownRemaining;
 
     private Animator popUpAnimator;
     private int count = 0;
@@ -34,6 +39,18 @@ public class PanelManager : MonoBehaviour
         finalScoreText.SetActive(false);
         countText.text = "Count: " + count.ToString();
         // popUpText = popUp.transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        enemyCooldownRemaining -= Time.deltaTime;
+        if (enemyCooldownRemaining > 0)
+        {
+            enemyClock.fillAmount = 1 - (enemyCooldownRemaining / enemyCooldown);
+        } else {
+            enemyClock.fillAmount = 1;
+            enemyCooldownRemaining = enemyCooldown;
+        }
     }
 
     private void IncreaseCount(GameObject pickup)
